c115a19dfe9cdbe6f9112c2702ac2fb5916d3dca
shraman <shraman@mit.edu>
2017-11-16 21:51:54 -0500

    refactor

--- vis_kps.hpp
+++ vis_kps.hpp
@@ -27,9 +27,8 @@ typedef vec3_t<float>   float3_t;
 typedef vec3_t<double> double3_t;
 typedef vec3_t<uint8_t> color_t;
 
-// TODO: make consistent - replace r,c with x,y
-inline int ijtoi(int r, int c, int width) {
-    return r*width + c;
+inline int ijtoi(int2_t pos, int width) {
+    return pos.y*width + pos.x;
 }
 
 inline int2_t itoij(int i, int width) {
@@ -46,18 +45,17 @@ inline float pix_diff(int3_t p1, int3_t p2) {
     return (xd*xd) + (yd*yd) + (zd*zd);
 }
 
-inline edge_t gen_edge(img_t& im, int r1, int c1, int r2, int c2, int width) {
-    // TODO: confirm c,r order
+inline edge_t gen_edge(img_t& im, int2_t pos1, int2_t pos2, int width) {
     int3_t p1 = {
-        im(c1, r1, 0, 0),
-        im(c1, r1, 0, 1),
-        im(c1, r1, 0, 2)};
+        im(pos1.x, pos1.y, 0, 0),
+        im(pos1.x, pos1.y, 0, 1),
+        im(pos1.x, pos1.y, 0, 2)};
     int3_t p2 = {
-        im(c2, r2, 0, 0),
-        im(c2, r2, 0, 1),
-        im(c2, r2, 0, 2)};
+        im(pos2.x, pos2.y, 0, 0),
+        im(pos2.x, pos2.y, 0, 1),
+        im(pos2.x, pos2.y, 0, 2)};
 
-    return {ijtoi(r1,c1,width), ijtoi(r2,c2,width), pix_diff(p1, p2)};
+    return {ijtoi(pos1, width), ijtoi(pos2, width), pix_diff(p1, p2)};
 }
 
 void extract_points(const char* path, vector<float2_t>& out) {
@@ -77,37 +75,34 @@ void build_graph(img_t& im, vector<edge_t>& out) {
     int h = im.height();
     int w = im.width();
 
-    for (int r = 1; r < h-1; ++r) {
-        for (int c = 1; c < w; ++c) {
-            out.push_back(gen_edge(im, r, c, r-1, c, w));
-            out.push_back(gen_edge(im, r, c, r-1, c-1, w));
-            out.push_back(gen_edge(im, r, c, r, c-1, w));
-            out.push_back(gen_edge(im, r, c, r+1, c-1, w));
+    for (int y = 1; y < h-1; ++y) {
+        for (int x = 1; x < w; ++x) {
+            out.push_back(gen_edge(im, {x,y}, {x, y-1}, w));
+            out.push_back(gen_edge(im, {x,y}, {x-1, y-1}, w));
+            out.push_back(gen_edge(im, {x,y}, {x-1, y}, w));
+            out.push_back(gen_edge(im, {x,y}, {x-1, y+1}, w));
         }
     }
 
     // bottom row
-    for (int c = 1; c < w; ++c) {
-        out.push_back(gen_edge(im, h-1, c, h-2, c, w));
-        out.push_back(gen_edge(im, h-1, c, h-2, c-1, w));
-        out.push_back(gen_edge(im, h-1, c, h-1, c-1, w));
+    for (int x = 1; x < w; ++x) {
+        out.push_back(gen_edge(im, {x, h-1}, {x, h-2}, w));
+        out.push_back(gen_edge(im, {x, h-1}, {x-1, h-2}, w));
+        out.push_back(gen_edge(im, {x, h-1}, {x-1, h-1}, w));
     }
 
     // top row
-    for (int c = 1; c < w; ++c) {
-        out.push_back(gen_edge(im, 0, c, 0, c-1, w));
-        out.push_back(gen_edge(im, 0, c, 1, c-1, w));
+    for (int x = 1; x < w; ++x) {
+        out.push_back(gen_edge(im, {x, 0}, {x-1, 0}, w));
+        out.push_back(gen_edge(im, {x, 0}, {x-1, 1}, w));
     }
 
     // left column
-    for (int r = 1; r < h; ++r) {
-        out.push_back(gen_edge(im, r, 0, r-1, 0, w));
+    for (int y = 1; y < h; ++y) {
+        out.push_back(gen_edge(im, {0, y}, {0, y-1}, w));
     }
 }
 
 __attribute__((always_inline)) double bench(clock_t& start) {
     return (clock() - start) / double(CLOCKS_PER_SEC);
 }
-
-
-
